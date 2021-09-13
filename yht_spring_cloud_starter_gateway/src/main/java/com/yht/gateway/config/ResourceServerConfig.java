9d8186a20607ca3761c6c7a6eb3afb70753f5a46
搬砖的猴子 <1475409096@qq.com>
2021-09-13 11:29:45 +0800

    更改配置

--- yht_spring_cloud_starter_gateway/src/main/java/com/yht/gateway/config/ResourceServerConfig.java
+++ yht_spring_cloud_starter_gateway/src/main/java/com/yht/gateway/config/ResourceServerConfig.java
@@ -9,13 +9,17 @@ import com.yht.gateway.filter.IgnoreUrlsRemoveJwtFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 /**
  * 网关对安全配置策略配置，资源服务器配置
@@ -75,7 +79,7 @@ public class ResourceServerConfig {
      * @return
      */
     @Bean
-    public ReactiveJwtAuthenticationConverterAdapter jwtAuthenticationConverter() {
+    public Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> jwtAuthenticationConverter() {
         JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
         jwtGrantedAuthoritiesConverter.setAuthorityPrefix(AuthConstant.AUTHORITY_PREFIX);
         jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName(AuthConstant.AUTHORITY_CLAIM_NAME);
